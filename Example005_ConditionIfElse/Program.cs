4568782d7e93a624b0b73154f29a957c08f15339
Alexfishkov <afscorp@mail.ru>
2022-11-30 12:00:25 +0300

    Example005_ConditIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Представьтесь пожалуйста> ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Урра!!! Это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет " + username);
+}
+
