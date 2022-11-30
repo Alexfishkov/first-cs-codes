c568cd4e0ebffb0b91f3db321046d801cbffc580
Alexfishkov <afscorp@mail.ru>
2022-11-30 12:19:22 +0300

    Example006_Maximum

---
+++ Example006_Max/Program.cs
@@ -0,0 +1,9 @@
+﻿int a=5, b=7, c=13, d=2, e=25;
+Console.WriteLine("Программой заданы 5 чисел: " + a + " " + b + " " + c + " " + d + " " + e);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine("Максимальное значение среди них: " + max);
